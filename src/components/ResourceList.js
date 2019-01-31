35ed91015d75b19ca7189850b4f12ee860acec04
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-01-30 20:10:32 -0600

    Lifecycles with useEffect

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function ResourceList(props) {
-  return <div>{props.resource}</div>;
+function ResourceList({ resource }) {
+  const [resources, setResources] = useState([]);
+  const fetchResource = async resource => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/${resource}`
+    );
+    setResources(response.data);
+  };
+  useEffect(async () => {
+    fetchResource(resource);
+  }, []);
+  return <div>{resources}</div>;
 }
 export default ResourceList;
