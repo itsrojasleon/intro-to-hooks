c590909a39e3fdcb785c2a4981630a425f778aa0
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-01-31 13:51:33 -0600

    Quick gotcha with useEffect

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -2,15 +2,19 @@ import React, { useState, useEffect } from 'react';
 
 function ResourceList({ resource }) {
   const [resources, setResources] = useState([]);
-  const fetchResource = async resource => {
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/${resource}`
-    );
-    setResources(response.data);
-  };
-  useEffect(async () => {
-    fetchResource(resource);
-  }, []);
-  return <div>{resources}</div>;
+
+  useEffect(
+    () => {
+      (async resource => {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/${resource}`
+        );
+        const data = await response.json();
+        setResources(data);
+      })(resource);
+    },
+    [resource]
+  );
+  return <div>{resources.length}</div>;
 }
 export default ResourceList;
